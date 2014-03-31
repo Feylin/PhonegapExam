adf575e9fedeeca8f8d6346d6dedbe4229de5a96
Feylin <spoler99@msn.com>
2014-03-31 13:46:04 +0200

    init

---
+++ Exam-CordovaLib/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+C:\Data\mobiledev\cordova\cordova-android\bin\exam\platforms\android\CordovaLib\ant-build\AndroidManifest.xml : \
+C:\Data\mobiledev\cordova\cordova-android\bin\exam\platforms\android\CordovaLib\AndroidManifest.xml \
---
+++ Exam-CordovaLib/ant-gen/R.java.d
@@ -0,0 +1,1 @@
+ : C:\Data\mobiledev\cordova\cordova-android\bin\exam\platforms\android\CordovaLib\ant-build\AndroidManifest.xml \
